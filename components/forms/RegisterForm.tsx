80e543e82655604aa346e20a40226e709cd8b636
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2025-09-08 15:14:38 +0300

    Emergency Contact Name & Emergency Contact Number

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -143,6 +143,30 @@ const RegisterForm = ({ user }: { user: User }) => {
               placeholder=" Software Engineer"
             />
           </div>
+          {/* Emergency Contact Name & Emergency Contact Number */}
+          <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+              fieldType={FormFieldType.INPUT}
+              control={form.control}
+              name="emergencyContactName"
+              label="Emergency contact name"
+              placeholder="Guardian's name"
+            />
+
+            <CustomFormField
+              fieldType={FormFieldType.PHONE_INPUT}
+              control={form.control}
+              name="emergencyContactNumber"
+              label="Emergency contact number"
+              placeholder="(555) 123-4567"
+            />
+          </div>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+          </div>
         </section>
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
